c410f99f4c0a6b1765c38caa7092122ef637cecd
Bryan Neves <bryan.neves@indt.org.br>
2025-08-20 21:21:24 -0400

    feat: add entities

--- src/main/java/com/eventostec/api/domain/event/Event.java
+++ src/main/java/com/eventostec/api/domain/event/Event.java
@@ -4,13 +4,25 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-
+import lombok.*;
+import java.util.Date;
 import java.util.UUID;
 
-@Entity()
 @Table(name = "event")
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Event {
     @Id
     @GeneratedValue
     private UUID id;
+
+    private String title;
+    private String description;
+    private String imgUrl;
+    private String eventUrl;
+    private Boolean remove;
+    private Date date;
 }
