ea0318506d49170b1c8dd94e88d9852584f41331
Bryan Neves <bryan.neves@indt.org.br>
2025-08-28 09:40:10 -0400

    feat: add aws config

--- src/main/java/com/eventostec/api/domain/event/EventRequestDto.java
+++ src/main/java/com/eventostec/api/domain/event/EventRequestDto.java
@@ -2,7 +2,5 @@ package com.eventostec.api.domain.event;
 
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.Date;
-
-public record EventRequestDto(String title, String description, Date date, String city, String state, Boolean remote, String eventUrl, MultipartFile image) {
+public record EventRequestDto(String title, String description, Long date, String city, String state, Boolean remote, String eventUrl, MultipartFile image) {
 }
