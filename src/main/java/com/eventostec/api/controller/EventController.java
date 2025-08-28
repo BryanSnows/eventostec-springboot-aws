22e1b053110f356d935aebf83bc9cf946619b823
Bryan Neves <bryan.neves@indt.org.br>
2025-08-28 11:42:28 -0400

    feat: add swagger config

--- src/main/java/com/eventostec/api/controller/EventController.java
+++ src/main/java/com/eventostec/api/controller/EventController.java
@@ -4,19 +4,23 @@ import com.eventostec.api.domain.event.Event;
 import com.eventostec.api.domain.event.EventRequestDto;
 import com.eventostec.api.service.EventService;
 import com.sun.jdi.request.EventRequest;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/event")
+@Tag(name = "Event Controller", description = "Gerenciamento de eventos")
 public class EventController {
 
     @Autowired
     private EventService eventService;
 
+    @PostMapping
     public ResponseEntity<Event> create(@RequestBody EventRequestDto body) {
         Event newEvent = this.eventService.createEvent(body);
         return ResponseEntity.ok(newEvent);
